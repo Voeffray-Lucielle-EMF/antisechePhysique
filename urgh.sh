eade6060551e72e006a94db1751ff9af912ee8a5
Voeffray-Lucielle-EMF <lucielle.voeffray@studentfr.ch>
2025-09-10 16:05:21 +0200

    AAAAAH

--- urgh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-s=("./PHY_Cheat_Sheet.aux" "./PHY_Cheat_Sheet.dvi" "./PHY_Cheat_Sheet.log" "./PHY_Cheat_Sheet.synctex.gz" "./texput.log" "./urgh.sh")
-
-for file in ${s[@]}; do
-  git rm --cached $file
-done
